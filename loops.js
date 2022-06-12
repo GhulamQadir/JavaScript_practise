a4e6be11c42af5aee942460a649598aecd38860d
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-06-12 14:51:41 -0700

    loops practise

---
+++ loops.js
@@ -0,0 +1,37 @@
+// for loop
+// var tableNum = window.prompt("Enter table number");
+// var tableInt = parseInt(tableNum);
+// for (var i = 1; i <= 10; i++) {
+//     console.log(`${tableInt} x ${i} = ${tableInt * i}`)
+// }
+
+
+// to find the factorial of any number
+var num = window.prompt("Enter factorial number");
+var convertNum = parseInt(num)
+var factorial = 1;
+for (var i = num; i >= 1; i--) {
+    factorial *= i;
+}
+console.log(factorial);
+
+
+// for loops boolean
+// var provinces = ["sindh", "punjab", "balochistan", "khyber pakhtun khuwa"];
+// var checkProvince = window.prompt("Enter your province name");
+// var changeCase = checkProvince.toLowerCase();
+// var boolVal = false;
+
+// for (var i = 0; i < provinces.length; i++) {
+
+//     if (changeCase === provinces[i]) {
+//         boolVal = true;
+//         console.log("This is the province of Pakistan")
+//         break;
+//     }
+// }
+// if (boolVal === false) {
+//     console.log("This is not the province of Pakistan")
+// }
+
+
