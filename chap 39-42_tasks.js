6e96bae94addb1c34ea5e2c2dc546df4d49db46c
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-06-18 14:04:48 -0700

    chapter 39-42 questions practise

--- chap 39-42_tasks.js
+++ chap 39-42_tasks.js
@@ -29,20 +29,50 @@
 
 
 
-// Q:If the lengths of the sides of a triangle are denoted by a, b, and c, then area of triangle is given by
+// Q:3If the lengths of the sides of a triangle are denoted by a, b, and c, then area of triangle is given by
+
+// function area(a, b, c) {
+//     var area;
+//     var s;
+//     function where() {
+//         s = (a + b + c) / 2
+//         return s;
+//     }
+//     where();
+//     area = s * (s - a) * (s - b) * (s - c);
+//     return area;
+
+// }
+
+// console.log(area(2, 5, 5))
 
 
-function area(a, b, c) {
-    var area;
-    var s;
-    function where() {
-        s = (a + b + c) / 2
-        return s;
-    }
-    where();
-    area = s * (s - a) * (s - b) * (s - c);
-    return area;
 
-}
+// Q:4. Write a function that receives marks received by a student in 3 subjects and returns the average and percentage of these
+// marks. there should be 3 functions one is the mainFunction and other are for average and percentage. Call those functions from mainFunction and display result in mainFunction.
+// var urdu = Number(prompt("Enter your Urdu marks"));
+// var eng = Number(prompt("Enter your English marks"));
+// var maths = Number(prompt("Enter your Maths marks"));
+// var marksObt = urdu + eng + maths;
+// var totalMarks = 225;
+// var percentage;
+// var average;
 
-console.log(area(2, 5, 5))
+// function getAverage() {
+//     average = (marksObt / 3).toFixed(2);
+//     return `Average: ${average}`;
+// }
+
+
+// function getPercentage() {
+//     percentage = ((marksObt / totalMarks) * 100).toFixed(2)
+//     return `Percentage: ${percentage}%`;
+// }
+
+// function mainFunction() {
+//     var func = [getAverage(), getPercentage()]
+//     for (var i = 0; i < func.length; i++) {
+//         document.write(`${func[i]}<br>`)
+//     }
+// }
+// mainFunction();
