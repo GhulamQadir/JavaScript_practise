f24e65b1d6fd4d5af36eaa89e84b32d529df82e3
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-07-06 13:04:43 -0700

    js practise

--- chap 76-80.js
+++ chap 76-80.js
@@ -16,7 +16,44 @@ function openFacebook() {
     window.location.assign('https://www.facebook.com/') //work same like href but this is best practise
 }
 
+
+// replacing url
 function login() {
     var email = document.getElementById('email')
     var password = document.getElementById('password')
+    if (email.value.length >= 9 && password.value >= 6) {
+        window.location.replace('file:///C:/Users/abdulqadir/Desktop/JS-practise/home.html')
+        alert("Your are logged in successfully")
+    }
+}
+
+
+
+
+
+// chap:78 Browser control: Filling the window with content
+// function openWindow() {
+//     // window.open()    //opens blank window
+
+//     window.open('https://www.linkedin.com')
+
+// }
+
+
+
+// chap: 80 Controlling the window's size and location
+// function openHomeFile() {
+//     window.open('home.html', 'win1', 'height=200, width=200,left=200,top=100')
+// }
+
+
+
+
+// chap 81: Browser control: Testing for popup blockers
+function checkForPopupBlocker() {
+    var win = window.open('https://github.com')
+    if (win === null) {
+        alert("Please enable popup locker")
+        win.close()
+    }
 }
